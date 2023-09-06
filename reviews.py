7cf30c74cdc28e53ce93f704f5a67ff8f29a81aa
Kevin Le <54592360+kevinle108@users.noreply.github.com>
2023-09-06 21:04:44 +0000

    value_counts for country

--- reviews.py
+++ reviews.py
@@ -5,4 +5,6 @@ A program that creates a summary of wine reviews from a csv file.
 import pandas as pd
 
 df = pd.read_csv('./data/winemag-data-130k-v2.csv')
-
+country_counts = df['country'].value_counts().reset_index()
+country_counts.columns = ['country', 'count']
+print(country_counts)
