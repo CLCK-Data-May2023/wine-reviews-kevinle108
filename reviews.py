64b56c703f370a9eb047bb458eb589acd557d058
Kevin Le <54592360+kevinle108@users.noreply.github.com>
2023-09-07 00:10:21 +0000

    cleaned up code

--- reviews.py
+++ reviews.py
@@ -5,13 +5,12 @@ A program that creates a summary of wine reviews from a csv file.
 import pandas as pd
 
 df = pd.read_csv('./data/winemag-data-130k-v2.csv')
-country_counts = df['country'].value_counts().reset_index()
-country_counts.columns = ['country', 'count']
+country_counts = df['country'].value_counts()
 # print(country_counts)
 
 points = df.groupby('country').points.mean().round(1)
 final = pd.merge(country_counts, points, on='country')
-# print(final)
+print(final)
 
 final.to_csv('data/reviews-per-country.csv')
 
